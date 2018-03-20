aa69ad472cc7d1099e9132134ebbd9266c50e958
Alexander Nissen <alexander@feldbech.dk>
2018-03-20 01:08:50 +0100

    Add comments to Pigeonhole Sort

--- src/PigeonholeSort.java
+++ src/PigeonholeSort.java
@@ -3,20 +3,34 @@ public class PigeonholeSort extends VisualSort {
         super("Pigeonhole Sort");
     }
 
+    /**
+     * Sort array with the Pigeonhole Sorting algorithm
+     * @param A - Array to sort
+     */
     public void doSort(int[] A) {
+        // Find minimum and maximum values in A
         MinMax mm = Util.getMinMax(A);
         int min = mm.getMin();
         int max = mm.getMax();
+
+        // Find range
         int range = max - min + 1;
 
+        // Create new array of buckets or empty "pigeon holes"
+        // Each hole will contain matching elements
         int[] phole = new int[range];
+
+        // Put every element in its respective hole
         for (int i = 0; i < A.length; i++) {
             phole[A[i] - min]++;
             visualize(A, i);
         }
-        int index = 0;
 
+        // Traverse through each hole
+        // Add the number of each element from each hole to array
+        int index = 0;
         for (int j = 0; j < range; j++) {
+            // Count down # of elements in this hole
             while (phole[j]-- > 0) {
                 A[index++] = j + min;
                 visualize(A);

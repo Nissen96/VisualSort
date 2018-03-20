62af3c91ad6c25f127daf96328e2a5faa4fe29af
Alexander Nissen <alexander@feldbech.dk>
2018-03-20 01:04:58 +0100

    Move utility methods to Util

--- src/VisualSort.java
+++ src/VisualSort.java
@@ -1,15 +1,5 @@
-import javax.imageio.ImageIO;
-import javax.sound.midi.MidiChannel;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Synthesizer;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 abstract public class VisualSort extends JFrame {
     // Each sorter should include this method
@@ -41,8 +31,8 @@ abstract public class VisualSort extends JFrame {
 
         // Get windows size
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        this.width = screenSize.width - 200;
-        this.height = screenSize.height - 200;
+        this.width = screenSize.width - 100;
+        this.height = screenSize.height - 100;
 
         // Store parameters
         this.hue = (float) Math.random();
@@ -51,6 +41,7 @@ abstract public class VisualSort extends JFrame {
         this.currentIndex = -1;
         this.buffer = 1;
         this.frame = 0;
+
         //this.tonePlayer = new TonePlayer();
 
         /*
@@ -116,7 +107,7 @@ abstract public class VisualSort extends JFrame {
 
         // Make a copy if array should not be sorted in place
         if (!inPlace) {
-            array = arrayCopy(array);
+            array = Util.arrayCopy(array);
         }
 
         // Call the sorting method of the sorter (any VisualSort extension)
@@ -125,85 +116,6 @@ abstract public class VisualSort extends JFrame {
         // Display that the array is sorted
         displaySorted(array);
         return array;
-    }
-
-    /**
-     * Copy an array
-     * @param array - The array to copy
-     * @return the copy of the array
-     */
-    static int[] arrayCopy(int[] array) {
-        // Make a new array of the same size as the old
-        int[] copy = new int[array.length];
-
-        // Copy and return the new array
-        System.arraycopy(array, 0, copy, 0, array.length);
-        return copy;
-    }
-
-    /**
-     * Swap two elements in an array
-     * @param array - The array to swap in
-     * @param i - The index of the first element
-     * @param j - The index of the second element
-     */
-    void swap(int[] array, int i, int j) {
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
-        visualize(array);
-    }
-
-    /**
-     * Check if an array is sorted
-     * @param array - The array to check
-     * @return whether or not the array is sorted
-     */
-    boolean isSorted(int[] array) {
-        // Check that no element is larger than the next
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Randomize an array in place
-     * @param array - Array to randomize
-     */
-    static void shuffle(int[] array) {
-        Random rnd = ThreadLocalRandom.current();
-
-        int idx, tmp;
-        // Iterate over array from the top and down
-        for (int i = array.length - 1; i > 0; i--) {
-            // Pick a random index to swap the current with
-            idx = rnd.nextInt(i + 1);
-
-            // Swap element at index idx and i
-            tmp = array[idx];
-            array[idx] = array[i];
-            array[i] = tmp;
-        }
-
-    }
-
-    /**
-     * Get the largest value in an array
-     * @param array - The array to search
-     * @return the largest value in the array
-     */
-    static int getMax(int[] array) {
-        // Initialize the largest value to the first
-        int max = array[0];
-
-        // Update max if any larger value is found
-        for (int i : array) {
-            if (i > max) max = i;
-        }
-        return max;
     }
 
     /**
@@ -230,15 +142,6 @@ abstract public class VisualSort extends JFrame {
     }
 
     /**
-     * Visualization method with curIdx set to -1
-     * This means no black bar will be displayed and is default
-     * @param array - Array to visualize
-     */
-    void visualize(int[] array) {
-        visualize(array, -1);
-    }
-
-    /**
      * Draw the visualization of the array
      * Wait for an optional amount of time
      * @param array - Array to visualize
@@ -261,6 +164,15 @@ abstract public class VisualSort extends JFrame {
     }
 
     /**
+     * Visualization method with curIdx set to -1
+     * This means no black bar will be displayed and is default
+     * @param array - Array to visualize
+     */
+    void visualize(int[] array) {
+        visualize(array, -1);
+    }
+
+    /**
      * Draw each value in the array as a bar with its value as its height
      * This corresponds to a single frame in the visualization of the sorting steps
      */
@@ -270,17 +182,13 @@ abstract public class VisualSort extends JFrame {
 
         // Calculate size of each coloured square
         int numElements = currentArray.length;
-        int squareWidth = (width - 200) / numElements;
-        int squareHeight = (height - 200) / numElements;
+        int squareWidth = (width - 100) / numElements;
+        int squareHeight = (height - 100) / numElements;
         int colHeight;
-
-        // Fix a bug
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 100, height + 200);
 
         // Go to each index in the array, and fill a bar
         // with the height and brightness representing the magnitude of the value.
-        for (int i = 0 ; i < numElements; i++ ) {
+        for ( int i = 0; i < numElements; i++ ) {
             // Height of the current column
             colHeight = squareHeight * currentArray[i];
 
@@ -300,8 +208,6 @@ abstract public class VisualSort extends JFrame {
             }
             g.fillRect(100 + i * squareWidth, height - 100 - colHeight, squareWidth, colHeight);
         }
-        //tonePlayer.play(440, 100);
-        //tonePlayer.close();
     }
 
     public void paint2(Graphics g) {

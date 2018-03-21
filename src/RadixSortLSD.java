d02e4d2bcf1fb262ae2af9a0384a7cc824201674
Alexander Nissen <alexander@feldbech.dk>
2018-03-21 17:03:11 +0100

    Move CountingSort subroutine to RadixSortLSD Moved since it is not used by RadixSortMSD

--- src/RadixSortLSD.java
+++ src/RadixSortLSD.java
@@ -22,4 +22,42 @@ abstract class RadixSortLSD extends RadixSort {
             countingSort(A, i);
         }
     }
+
+    /**
+     * Sort array of numbers on a specific digit with Counting Sort
+     * @param A - The array to sort
+     * @param digit - The digit to sort by
+     */
+    private void countingSort(int[] A, int digit) {
+        // Make a copy of A and a counter C which stores the count of each digit
+        // The numbers are in the given base and C therefore only needs to store base positions
+        int base = getBase();
+        int[] B = Util.arrayCopy(A);
+        int[] C = new int[base];
+        int digitOfNumber;
+
+        // Count the number of occurrences of each digit in B
+        for (int i = 0; i < B.length; i++) {
+            // Get the digit from B on position 'digit'
+            digitOfNumber = (int) (B[i] / Math.pow(base, digit)) % base;
+            C[digitOfNumber]++;
+            visualize(A, i);
+        }
+
+        // Compute the cumulative # of digits of C
+        for (int i = 1; i < base; i++) {
+            C[i] += C[i - 1];
+        }
+
+        // Iterate backwards over B (keeps the sort stable)
+        // Insert the values from B in their correct spot in A (found in C)
+        for (int i = A.length - 1; i >= 0; i--) {
+            // Get the digit from B on position 'digit'
+            digitOfNumber = (int) (B[i] / Math.pow(base, digit)) % base;
+            C[digitOfNumber]--;
+            A[C[digitOfNumber]] = B[i];
+
+            visualize(A);
+        }
+    }
 }
